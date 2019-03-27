cac5e337cf906a91deb80ec61eed53021ea14d4c
Felipe <jfpsb_felipe2@hotmail.com>
2019-03-27 09:49:16 -0300

    ndamas2

--- AtividadeNDamas2/Program.cs
+++ AtividadeNDamas2/Program.cs
@@ -5,25 +5,43 @@ namespace AtividadeNDamas
     class Program
     {
         static int cont = 1;
+        static int casos;
+        static string[] linhacoluna;
 
         static void Main(string[] args)
         {
             cont = 1;
 
-            NDamas(new int[8], 0, 8);
+            casos = int.Parse(Console.ReadLine());
+
+            int[] linha = new int[casos];
+            int[] coluna = new int[casos];
+
+            for (int i = 0; i < casos; i++)
+            {
+                linhacoluna = Console.ReadLine().Split();
+                linha[i] = int.Parse(linhacoluna[0]);
+                coluna[i] = int.Parse(linhacoluna[1]);
+            }
+
+            Console.WriteLine("SOLN" + '\t' + "   COLUMN");
+            Console.WriteLine(" #" + '\t' + "1 2 3 4 5 6 7 8\n");
+
+            for (int i = 0; i < linha.Length; i++)
+                NDamas(new int[8], 0, 8, linha[i], coluna[i]);
 
             Console.ReadKey();
         }
 
-        public static void NDamas(int[] vetor, int index, int n)
+        public static void NDamas(int[] vetor, int index, int n, int linha, int coluna)
         {
             if (index == n)
             {
-                Console.Write(cont++ + ": " + vetor[0]);
+                Console.Write(" " + cont++ + '\t' + vetor[0]);
 
                 for (int i = 1; i < n; i++)
                 {
-                    Console.Write(vetor[i]);
+                    Console.Write(" " + vetor[i]);
                 }
                 Console.WriteLine();
             }
@@ -49,10 +67,13 @@ namespace AtividadeNDamas
                         }
                     }
 
+                    if (ok && (index + 1) == coluna && i != linha)
+                        ok = false;
+
                     if (ok)
                     {
                         vetor[index] = i;
-                        NDamas(vetor, index + 1, n);
+                        NDamas(vetor, index + 1, n, linha, coluna);
                     }
                 }
             }
