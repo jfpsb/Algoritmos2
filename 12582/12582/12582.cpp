4d198eaae6f95da4b35fa7d98ede286866b3fb8b
Felipe <jfpsb_felipe2@hotmail.com>
2019-06-12 15:07:22 -0300

    12582 Funcionando

--- 12582/12582/12582.cpp
+++ 12582/12582/12582.cpp
@@ -4,31 +4,10 @@
 
 using namespace std;
 
-int charToIndex(char c) {
-	switch (c)
-	{
-	case 'A':
-		return 0;
-	case 'B':
-		return 1;
-	case 'C':
-		return 2;
-	case 'D':
-		return 3;
-	case 'E':
-		return 4;
-	case 'F':
-		return 5;
-	case 'G':
-		return 6;
-	}
-}
-
 int main()
 {
-	int t, index, paths[7] = { 0 };
+	int t, index;
 	char sprinklerseq[1000];
-	stack <char> s;
 
 	scanf("%d", &t);
 
@@ -37,38 +16,34 @@ int main()
 	for (int i = 0; i < t; i++) {
 		fgets(sprinklerseq, 1000, stdin);
 
-		index = 0;
+		stack <char> s;
+		int paths[26] = { 0 };
+
+		s.push(sprinklerseq[0]);
+
+		index = 1;
 
 		while (sprinklerseq[index] != '\0') {
-			//printf("%c", sprinklerseq[index]);
-			if (index == 0) {
-				s.push(sprinklerseq[index]);
-				paths[charToIndex(sprinklerseq[index])] += 1;
+			char top = s.top();
+
+			if (sprinklerseq[index] == top) {
+				if (s.size() != 1) {
+					s.pop();
+					paths[s.top() - 'A'] += 1;
+				}
 			}
 			else {
-				if (s.size() != 0) {
-					char top = s.top();
-
-					printf("TOP: %c\n", top);
-
-					if (sprinklerseq[index] == top) {
-						s.pop();
-
-						top = s.top();
-						paths[charToIndex(top)] += 1;
-					}
-					else {
-						s.push(sprinklerseq[index]);
-						paths[charToIndex(sprinklerseq[index])] += 1;
-					}
-				}
+				s.push(sprinklerseq[index]);
+				paths[sprinklerseq[index] - 'A'] += 1;
 			}
 
 			index++;
 		}
 
-		for (int j = 0; j < 7; j++) {
-			printf("%d\n", paths[j]);
+		printf("Case %d\n", i + 1);
+		for (int j = 0; j < 26; j++) {
+			if(paths[j] != 0)
+				printf("%c = %d\n", 'A' + j, paths[j]);
 		}
 	}
 }
